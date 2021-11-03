71b50547f52315fa122d296d570dab6ed19e317e
ahuelhorst <abbyhuelhorst@gmail.com>
2021-11-02 21:17:36 -0400

    Refactor encodeIngredientString method

--- src/main/java/edu/bsu/cs222/UrlBuilder.java
+++ src/main/java/edu/bsu/cs222/UrlBuilder.java
@@ -2,8 +2,11 @@ package edu.bsu.cs222;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class UrlBuilder {
@@ -14,18 +17,19 @@ public class UrlBuilder {
 
     public UrlBuilder() throws FileNotFoundException {
     }
-    public URL buildIngredientUrl(String ingredients) throws MalformedURLException {
+    public URL buildIngredientUrl(String ingredients) throws MalformedURLException, UnsupportedEncodingException {
         String queryString = encodeIngredientString(ingredients);
         String urlString = String.format("https://api.spoonacular.com/recipes/findByIngredients?" +
                 "apiKey=%s&ingredients=%s&number=1", key, queryString);
         return new URL(urlString);
     }
-    public URL buildRecipeUrl(String recipeId) throws MalformedURLException {
+    public URL buildRecipeUrl(String recipeId) throws MalformedURLException, UnsupportedEncodingException {
+        String queryString = encodeIngredientString(recipeId);
         String urlString = String.format("https://api.spoonacular.com/recipes/%s/information?" +
-                "apiKey=%s&number=1",recipeId, key);
+                "apiKey=%s&number=1",queryString, key);
         return new URL(urlString);
     }
-    public String encodeIngredientString(String ingredients){
-        return ingredients.replaceAll(" ", "+");
+    public String encodeIngredientString(String query) throws UnsupportedEncodingException {
+        return URLEncoder.encode(query, StandardCharsets.UTF_8.toString());
     }
 }
