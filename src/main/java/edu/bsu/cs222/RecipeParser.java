894a325ed5978b161a04ed3162a4915a02e5b762
ahuelhorst <abbyhuelhorst@gmail.com>
2021-11-08 19:16:08 -0500

    Create idList array and refactor parseRecipeId method

--- src/main/java/edu/bsu/cs222/RecipeParser.java
+++ src/main/java/edu/bsu/cs222/RecipeParser.java
@@ -2,20 +2,28 @@ package edu.bsu.cs222;
 
 import com.jayway.jsonpath.JsonPath;
 import net.minidev.json.JSONArray;
-
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class RecipeParser {
-    public String parseRecipeId(String data) throws IOException {
-        JSONArray id = JsonPath.read(data, "$..id");
-        return id.get(0).toString();
+
+    private final ArrayList<String> idList = new ArrayList<>();
+
+    public ArrayList<String> parseRecipeId(String data) {
+        for (int i = 0; i<=5; i++){
+            String jsonPath = String.format("$[%d].id", i);
+            JSONArray id = JsonPath.read(data, jsonPath);
+            idList.add(id.get(i).toString());
+        }
+        return idList;
     }
 
-    public String parseRecipeTitle(String data) throws IOException {
+    public String parseRecipeTitle(String data) {
         JSONArray titles = JsonPath.read(data, "$..title");
         return titles.get(0).toString();
     }
+
     public URL parseRecipeSource(String data) throws IOException {
         JSONArray sources = JsonPath.read(data, "$..sourceUrl");
         String recipeSource = sources.get(0).toString();
