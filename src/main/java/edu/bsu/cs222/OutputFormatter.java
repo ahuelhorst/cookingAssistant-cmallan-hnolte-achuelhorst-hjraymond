1b6fe103d1b0361316f893f9025cb0e40b11f121
ahuelhorst <abbyhuelhorst@gmail.com>
2021-11-11 13:09:15 -0500

    Create formatNutrition method

--- src/main/java/edu/bsu/cs222/OutputFormatter.java
+++ src/main/java/edu/bsu/cs222/OutputFormatter.java
@@ -1,11 +1,22 @@
 package edu.bsu.cs222;
 
-public class OutputFormatter {
-    public String formatRecipe(Recipe recipe) {
-        return (String.format("Recipe Title: %s\nRecipe Link: %s", recipe.getTitle(), recipe.getSource()));
-    }
+import java.util.ArrayList;
 
-//    public String formatNutritionFacts(){
-//        return (String.format("Calories: %s\nCarbohydrates: %s\nProtein: %s\nFat: %s", ));
-//    }
+public class OutputFormatter {
+    private final StringBuilder stringBuilder = new StringBuilder();
+
+    public String format(ArrayList<Recipe> recipes){
+        for (Recipe recipe : recipes){
+            String title = String.format("Title: %s\nSource: %s\n\n", recipe.getTitle(), recipe.getSource());
+            stringBuilder.append(title);
+        }
+        return stringBuilder.toString();
+    }
+    public String formatNutrition(ArrayList<Recipe> recipes){
+        for (Recipe recipe : recipes){
+            String calories = String.format("Calories: %s\n\n", recipe.getCalories());
+            stringBuilder.append(calories);
+        }
+        return stringBuilder.toString();
+    }
 }
