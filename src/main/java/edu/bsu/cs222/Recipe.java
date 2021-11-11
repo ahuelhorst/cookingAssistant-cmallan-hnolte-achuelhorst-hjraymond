cff5c471c91824ffc129157a6be579bb8738fb49
ahuelhorst <abbyhuelhorst@gmail.com>
2021-11-11 13:11:44 -0500

    Refactor Recipe class

--- src/main/java/edu/bsu/cs222/Recipe.java
+++ src/main/java/edu/bsu/cs222/Recipe.java
@@ -3,69 +3,60 @@ package edu.bsu.cs222;
 import java.net.URL;
 
 public class Recipe {
-    public static class Builder{
+    public static class Builder {
         private String title;
         private String id;
-        private String calorie;
-        private String fat;
-        private String carb;
-        private String protein;
         private URL source;
+        private String calories;
+        private String fat;
 
-        public Builder withTitle(String title){
+        public Builder withTitle(String title) {
             this.title = title;
             return this;
         }
-        public Builder andId(String id){
+        public Builder andId(String id) {
             this.id = id;
             return this;
         }
-        public Builder andCalorie(String calorie){
-            this.calorie = calorie;
+        public Builder andCalories(String calories){
+            this.calories = calories;
             return this;
         }
         public Builder andFat(String fat){
             this.fat = fat;
             return this;
         }
-        public Builder andCarb(String carb){
-            this.carb = carb;
-            return this;
-        }
-        public Builder andProtein(String protein){
-            this.protein = protein;
-            return this;
-        }
-
         public Recipe andSource(URL source){
             this.source = source;
             return new Recipe(this);
         }
-
     }
     private final String title;
     private final String id;
     private final URL source;
-    private final String calorie;
+    private final String calories;
     private final String fat;
-    private final String carb;
-    private final String protein;
 
-    public Recipe(Builder builder){
+    public Recipe(Builder builder) {
         this.title = builder.title;
         this.id = builder.id;
-        this.source = builder.source;
-        this.calorie = builder.calorie;
+        this.calories = builder.calories;
         this.fat = builder.fat;
-        this.carb =  builder.fat;
-        this.protein = builder.protein;
+        this.source = builder.source;
     }
-
     public String getTitle() {
         return title;
     }
-
+    public String getId(){
+        return id;
+    }
     public URL getSource() {
         return source;
+    }
+    public String getCalories() {
+        return calories;
+    }
+    public String getFat() {
+        return fat;
     }
 }
