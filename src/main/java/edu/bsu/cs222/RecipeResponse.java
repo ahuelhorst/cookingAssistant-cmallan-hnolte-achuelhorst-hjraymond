1836ba6af63b602a6b533e1a84a3cb511964b200
ahuelhorst <abbyhuelhorst@gmail.com>
2021-11-11 13:08:08 -0500

    Refactor RecipeResponse

--- src/main/java/edu/bsu/cs222/RecipeResponse.java
+++ src/main/java/edu/bsu/cs222/RecipeResponse.java
@@ -2,19 +2,42 @@ package edu.bsu.cs222;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class RecipeResponse {
 
     RecipeParser parser = new RecipeParser();
     NutritionParser nutritionParser = new NutritionParser();
+    ArrayList<Recipe> recipeArrayList = new ArrayList<>();
 
-    public Recipe getResponse(String data) throws IOException {
-        String title = parser.parseRecipeTitle(data);
-        URL source = parser.parseRecipeSource(data);
-        String calorie = nutritionParser.parseCalories(data);
-        String fat = nutritionParser.parseFat(data);
-        String carb = nutritionParser.parseCarbs(data);
-        String protein = nutritionParser.parseProtein(data);
-        return new Recipe.Builder().withTitle(title).andSource(source);
+    public ArrayList<Recipe> getRecipes(String data) throws IOException {
+
+        ArrayList<String> titles = parser.parseRecipeTitle(data);
+        ArrayList<String> ids = parser.parseRecipeId(data);
+        for (int i = 0; i <= 4; i++) {
+            String title = titles.get(i);
+            String id = ids.get(i);
+            URL sourceUrl = getSource(id);
+            String calories = getCalories(id);
+            Recipe recipe = new Recipe.Builder().withTitle(title).andId(id).andCalories(calories).andSource(sourceUrl);
+            recipeArrayList.add(recipe);
+        }
+        return recipeArrayList;
+    }
+
+    public URL getSource(String id) throws IOException {
+        UrlBuilder urlBuilder = new UrlBuilder();
+        UrlConnector connector = new UrlConnector();
+        URL sourceUrl = urlBuilder.buildRecipeUrl(id);
+        String data = connector.openConnection(sourceUrl);
+        return parser.parseRecipeSource(data);
+    }
+
+    public String getCalories(String id) throws IOException {
+        UrlBuilder urlBuilder = new UrlBuilder();
+        UrlConnector connector = new UrlConnector();
+        URL nutritionUrl = urlBuilder.buildNutritionUrl(id);
+        String data = connector.openConnection(nutritionUrl);
+        return nutritionParser.parseCalories(data);
     }
 }
