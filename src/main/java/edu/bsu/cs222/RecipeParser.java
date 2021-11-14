738d1fbeb6c03a84a885286f4b368c97b9af99c9
ahuelhorst <abbyhuelhorst@gmail.com>
2021-11-13 23:01:19 -0500

    Adjust spacing

--- src/main/java/edu/bsu/cs222/RecipeParser.java
+++ src/main/java/edu/bsu/cs222/RecipeParser.java
@@ -7,10 +7,17 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class RecipeParser {
+
     private final ArrayList<String> idList = new ArrayList<>();
     private final ArrayList<String> titleList = new ArrayList<>();
-    private final ArrayList<URL> urlList = new ArrayList<>();
 
+    public ArrayList<String> parseRecipeTitle(String data) {
+        for (int i = 0; i<=4; i++){
+            JSONArray titles = JsonPath.read(data, "$..title");
+            titleList.add(titles.get(i).toString());
+        }
+        return titleList;
+    }
     public ArrayList<String> parseRecipeId(String data) {
         for (int i = 0; i<=4; i++){
             String jsonPath = String.format("$[%d].id", i);
@@ -20,17 +27,9 @@ public class RecipeParser {
         }
         return idList;
     }
-    public ArrayList<String> parseRecipeTitle(String data) {
-        for (int i = 0; i<=4; i++){
-            JSONArray titles = JsonPath.read(data, "$..title");
-            titleList.add(titles.get(i).toString());
-        }
-        return titleList;
-    }
     public URL parseRecipeSource(String data) throws IOException {
         JSONArray sources = JsonPath.read(data, "$..sourceUrl");
         String recipeSource = sources.get(0).toString();
-        URL recipeUrl = new URL(recipeSource);
-        return recipeUrl;
+        return new URL(recipeSource);
     }
 }
