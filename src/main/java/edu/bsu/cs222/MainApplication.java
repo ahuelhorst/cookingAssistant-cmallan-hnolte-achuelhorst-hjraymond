0e3e8e6cae38e4fcc57e4850e1bbc27989e84a3a
abbyhuelhorst <achuelhorst@bsu.edu>
2021-11-15 00:00:31 -0500

    Make inline variables

--- src/main/java/edu/bsu/cs222/MainApplication.java
+++ src/main/java/edu/bsu/cs222/MainApplication.java
@@ -5,23 +5,27 @@ import java.net.URL;
 import java.util.ArrayList;
 
 public class MainApplication {
+
     public String connectIngredientUrl(String userInput) throws IOException {
         UrlBuilder urlBuilder = new UrlBuilder();
         URL ingredientUrl = urlBuilder.buildIngredientUrl(userInput);
         UrlConnector connector = new UrlConnector();
-        String ingredientData = connector.openConnection(ingredientUrl);
-        return ingredientData;
+        return connector.openConnection(ingredientUrl);
     }
     public String processRecipes(String userInput) throws IOException {
         String data = connectIngredientUrl(userInput);
         ArrayList<Recipe> recipeList = createRecipeList(data);
         OutputFormatter formatter = new OutputFormatter();
-        String formattedRecipes = formatter.format(recipeList);
-        return formattedRecipes;
+        return formatter.format(recipeList);
+    }
+    public String processNutrition(String userInput) throws IOException {
+        String data = connectIngredientUrl(userInput);
+        ArrayList<Recipe> recipeList = createRecipeList(data);
+        OutputFormatter formatter = new OutputFormatter();
+        return formatter.formatNutrition(recipeList);
     }
     public ArrayList<Recipe> createRecipeList(String data) throws IOException {
         RecipeResponse recipeResponse = new RecipeResponse();
-        ArrayList<Recipe> recipeList = recipeResponse.getRecipes(data);
-        return recipeList;
+        return recipeResponse.getRecipes(data);
     }
 }
