886b3b22fedb25e703dc7c01a9fb6b62dc673eb0
ahuelhorst <abbyhuelhorst@gmail.com>
2021-11-30 19:33:43 -0500

    Reformat

--- src/test/java/edu/bsu/cs222/UrlBuilderTest.java
+++ src/test/java/edu/bsu/cs222/UrlBuilderTest.java
@@ -3,29 +3,32 @@ package edu.bsu.cs222;
 import edu.bsu.cs222.model.UrlBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class UrlBuilderTest {
 
-    private UrlBuilder urlBuilder = new UrlBuilder();
-    BufferedReader br = new BufferedReader(new InputStreamReader((Thread.currentThread().getContextClassLoader().getResourceAsStream( "key.txt" ))));
+    private final UrlBuilder urlBuilder = new UrlBuilder();
+    BufferedReader br = new BufferedReader(new InputStreamReader((Thread.currentThread().getContextClassLoader().getResourceAsStream("key.txt"))));
     String key = (br.readLine());
 
     public UrlBuilderTest() throws IOException {
     }
+
     @Test
     public void testIngredientUrlBuilder() throws MalformedURLException, UnsupportedEncodingException {
         String urlString = String.format("https://api.spoonacular.com/recipes/findByIngredients?" +
-                "apiKey=%s&ingredients=peanut+butter%s+flour%s+sugar&number=1", key, "%2C", "%2C");
+                "apiKey=%s&ingredients=peanut+butter%s+flour%s+sugar&number=5", key, "%2C", "%2C");
         URL ingredientUrl = new URL(urlString);
         URL actualUrl = urlBuilder.buildIngredientUrl("peanut butter, flour, sugar");
         Assertions.assertEquals(ingredientUrl, actualUrl);
     }
+
     @Test
     public void testRecipeUrlBuilder() throws MalformedURLException, UnsupportedEncodingException {
-        String urlString = String.format("https://api.spoonacular.com/recipes/1161746/information?apiKey=%s&number=1", key);
+        String urlString = String.format("https://api.spoonacular.com/recipes/1161746/information?apiKey=%s&number=5", key);
         URL recipeUrl = new URL(urlString);
         URL actualUrl = urlBuilder.buildRecipeUrl("1161746");
         Assertions.assertEquals(recipeUrl, actualUrl);
