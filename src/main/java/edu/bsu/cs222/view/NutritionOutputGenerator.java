2b6786e8b02bf2cfa96343d966f048ff21fb0221
abbyhuelhorst <achuelhorst@bsu.edu>
2021-12-09 11:16:52 -0500

    Refactor NutritionOutputGenerator

--- src/main/java/edu/bsu/cs222/view/NutritionOutputGenerator.java
+++ src/main/java/edu/bsu/cs222/view/NutritionOutputGenerator.java
@@ -1,5 +1,40 @@
 package edu.bsu.cs222.view;
 
+import edu.bsu.cs222.model.Recipe;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+
 public class NutritionOutputGenerator {
+
+    public VBox createNutritionOutput(Recipe recipe) {
+        VBox nutritionOutputBox = new VBox();
+        nutritionOutputBox.setSpacing(5);
+        nutritionOutputBox.setBackground(new Background(new BackgroundFill(Color.LIGHTSTEELBLUE, null, null)));
+        nutritionOutputBox.setPadding(new Insets(0, 5, 0, 0));
+        nutritionOutputBox.setPrefSize(300, 200);
+        nutritionOutputBox.getChildren().add(createLabelBox(recipe));
+        return nutritionOutputBox;
+    }
+
+    public Label createNutritionLabel() {
+        return new Label("Nutrition Info:");
+    }
+
+    public Label createNutritionFactsLabel(Recipe recipe) {
+        Label calorieLabel = new Label();
+        calorieLabel.setText("Calories: " + recipe.getCalories());
+        return calorieLabel;
+    }
+
+    public VBox createLabelBox(Recipe recipe) {
+        VBox labelBox = new VBox();
+        labelBox.getChildren().addAll(createNutritionLabel(), createNutritionFactsLabel(recipe));
+        return labelBox;
+    }
 
 }
