4d68148b73145192fe8644110c23da0874826a89
abbyhuelhorst <achuelhorst@bsu.edu>
2021-12-09 11:03:04 -0500

    Create testCheckApiKeyLocation method and checkApiKeyLocation

--- src/main/java/edu/bsu/cs222/model/ApiKey.java
+++ src/main/java/edu/bsu/cs222/model/ApiKey.java
@@ -2,5 +2,7 @@ package edu.bsu.cs222.model;
 
 public class ApiKey {
 
-
+    public Boolean checkFileLocation() {
+        return true;
+    }
 }
--- src/test/java/edu/bsu/cs222/ApiKeyTest.java
+++ src/test/java/edu/bsu/cs222/ApiKeyTest.java
@@ -1,5 +1,13 @@
 package edu.bsu.cs222;
 
+import edu.bsu.cs222.model.ApiKey;
+import org.junit.jupiter.api.Assertions;
+
 public class ApiKeyTest {
 
+    public void testCheckApiKeyLocation() {
+        ApiKey apiKey = new ApiKey();
+        Boolean booleanValue = apiKey.checkFileLocation();
+        Assertions.assertEquals(true, booleanValue);
+    }
 }
