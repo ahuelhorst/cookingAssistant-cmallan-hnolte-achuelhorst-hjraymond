ec2716c872c5405b8912389949dc2f023452287a
abbyhuelhorst <achuelhorst@bsu.edu>
2021-12-12 13:40:02 -0500

    Refactor checkFileLocation method and test

--- src/main/java/edu/bsu/cs222/model/ApiKey.java
+++ src/main/java/edu/bsu/cs222/model/ApiKey.java
@@ -3,6 +3,6 @@ package edu.bsu.cs222.model;
 public class ApiKey {
 
     public Boolean checkFileLocation() {
-        return true;
+        return ClassLoader.getSystemResource("key.txt") != null;
     }
 }
--- src/test/java/edu/bsu/cs222/ApiKeyTest.java
+++ src/test/java/edu/bsu/cs222/ApiKeyTest.java
@@ -2,9 +2,11 @@ package edu.bsu.cs222;
 
 import edu.bsu.cs222.model.ApiKey;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class ApiKeyTest {
 
+    @Test
     public void testCheckApiKeyLocation() {
         ApiKey apiKey = new ApiKey();
         Boolean booleanValue = apiKey.checkFileLocation();
