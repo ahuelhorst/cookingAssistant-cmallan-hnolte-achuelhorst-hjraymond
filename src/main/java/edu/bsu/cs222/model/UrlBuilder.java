8a757ab6f494655db8401a783065e2f7b7b5106f
abbyhuelhorst <achuelhorst@bsu.edu>
2021-12-12 13:45:44 -0500

    Remove apiKey from UrlBuilder and UrlBuilderTest

--- src/main/java/edu/bsu/cs222/model/UrlBuilder.java
+++ src/main/java/edu/bsu/cs222/model/UrlBuilder.java
@@ -1,37 +1,35 @@
 package edu.bsu.cs222.model;
 
-import java.io.*;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Objects;
+
 
 public class UrlBuilder {
 
-    BufferedReader br = new BufferedReader(new InputStreamReader((Objects.requireNonNull(Thread.currentThread().getContextClassLoader().getResourceAsStream("key.txt")))));
-    String key = (br.readLine());
-
-    public UrlBuilder() throws IOException {
-    }
     public URL buildIngredientUrl(String ingredients) throws MalformedURLException, UnsupportedEncodingException {
         String queryString = encodeString(ingredients);
         String urlString = String.format("https://api.spoonacular.com/recipes/findByIngredients?" +
-                "apiKey=%s&ingredients=%s&number=5", key, queryString);
+                "ingredients=%s&number=5", queryString);
         return new URL(urlString);
     }
+
     public URL buildRecipeUrl(String recipeId) throws MalformedURLException, UnsupportedEncodingException {
         String queryString = encodeString(recipeId);
         String urlString = String.format("https://api.spoonacular.com/recipes/%s/information?" +
-                "apiKey=%s&number=5",queryString, key);
+                "number=5", queryString);
         return new URL(urlString);
     }
+
     public URL buildNutritionUrl(String recipeId) throws UnsupportedEncodingException, MalformedURLException {
         String queryString = encodeString(recipeId);
         String urlString = String.format("https://api.spoonacular.com/recipes/%s/nutritionWidget.json?" +
-                "apiKey=%s&number=5", queryString, key);
+                "number=5", queryString);
         return new URL(urlString);
     }
+
     public String encodeString(String query) throws UnsupportedEncodingException {
         return URLEncoder.encode(query, StandardCharsets.UTF_8.toString());
     }
--- src/test/java/edu/bsu/cs222/UrlBuilderTest.java
+++ src/test/java/edu/bsu/cs222/UrlBuilderTest.java
@@ -4,23 +4,19 @@ import edu.bsu.cs222.model.UrlBuilder;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class UrlBuilderTest {
 
     private final UrlBuilder urlBuilder = new UrlBuilder();
-    BufferedReader br = new BufferedReader(new InputStreamReader((Thread.currentThread().getContextClassLoader().getResourceAsStream("key.txt"))));
-    String key = (br.readLine());
 
-    public UrlBuilderTest() throws IOException {
-    }
 
     @Test
     public void testIngredientUrlBuilder() throws MalformedURLException, UnsupportedEncodingException {
         String urlString = String.format("https://api.spoonacular.com/recipes/findByIngredients?" +
-                "apiKey=%s&ingredients=peanut+butter%s+flour%s+sugar&number=5", key, "%2C", "%2C");
+                "ingredients=peanut+butter%s+flour%s+sugar&number=5", "%2C", "%2C");
         URL ingredientUrl = new URL(urlString);
         URL actualUrl = urlBuilder.buildIngredientUrl("peanut butter, flour, sugar");
         Assertions.assertEquals(ingredientUrl, actualUrl);
@@ -28,7 +24,7 @@ public class UrlBuilderTest {
 
     @Test
     public void testRecipeUrlBuilder() throws MalformedURLException, UnsupportedEncodingException {
-        String urlString = String.format("https://api.spoonacular.com/recipes/1161746/information?apiKey=%s&number=5", key);
+        String urlString = "https://api.spoonacular.com/recipes/1161746/information?number=5";
         URL recipeUrl = new URL(urlString);
         URL actualUrl = urlBuilder.buildRecipeUrl("1161746");
         Assertions.assertEquals(recipeUrl, actualUrl);
